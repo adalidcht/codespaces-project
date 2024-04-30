90cabf374f4695f841dd7ed370168cbcf16843ae
Adalid Chávez Torreblanca <adalidcht@gmail.com>
2024-04-30 23:46:12 +0000

    change profile aboutme

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Adalid Chávez Torreblanca",
+  title: "Bionic Engineer | AI Developer",
+  email: "adalidcht@gmail.com",
+  gitHub: "adalidcht",
+  instagram: "",
+  linkedIn: "adalidchavez",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
